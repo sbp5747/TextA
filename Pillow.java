bbbe4b4c67ccff75c1d79d0de591684c3bc44c46
sbp5747 <123059520+sbp5747@users.noreply.github.com>
2023-01-30 00:04:24 -0500

    Add files via upload

---
+++ Pillow.java
@@ -0,0 +1,11 @@
+public class Pillow extends Weapon{
+
+    public Pillow() {
+        super("Pillow", "A pillow super soft", 1, 1);
+    }
+
+    public Pillow(String name, String description, int value, int damage) {
+        super(name, description, value, damage);
+    }
+
+}
