90e69cfec76d0bbc7ca5e000b336ef0cd0154200
sbp5747 <123059520+sbp5747@users.noreply.github.com>
2023-01-30 00:04:08 -0500

    Add files via upload

---
+++ Mace.java
@@ -0,0 +1,10 @@
+public class Mace extends Weapon {
+
+    public Mace() {
+        super("Mace", "A Mace. Somewhat more dangerous than a Sword" , 10, 30 );
+    }
+
+    public Mace(String name, String description, int value, int damage) {
+        super(name, description, value, damage);
+    }
+}
