a79476e084f53973210a568884dea5d3efde7d07
sbp5747 <123059520+sbp5747@users.noreply.github.com>
2023-01-30 00:04:49 -0500

    Add files via upload

---
+++ Sword.java
@@ -0,0 +1,11 @@
+public class Sword extends Weapon {
+
+    public Sword() {
+        super("Sword", "A Sword with some rust. Somewhat more dangerous than a dagger", 10, 20 );
+    }
+
+    public Sword(String name, String description, int value,  int damage) {
+        super(name, description, value, damage);
+    }
+
+}
